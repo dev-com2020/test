0d04149578a0c0c6b6ac719e5d7f87754929b301
DEV-COM <kwp@tlen.pl>
2022-03-07 09:26:36 +0100

    pon

--- wyjatki.php
+++ wyjatki.php
@@ -7,15 +7,41 @@
 // }
 
 
-try {
-    throw new Exception("Testowy wyjątek", 2021);
-} catch (Exception $e) {
-    echo "Został wygenerowany wyjątek! :)<br>";
-    echo "Treść komunikatu: ". $e->getMessage() . "<br>";
-    echo "Treść nazwa pliku: ". $e->getFile() . "<br>";
-    echo "Treść numer linii: ". $e->getLine() . "<br>";
-    echo "Treść kod błędu: ". $e->getCode() . "<br>";  
-}
+// try {
+//     throw new Exception("Testowy wyjątek", 2021);
+// } catch (Exception $e) {
+//     echo "Został wygenerowany wyjątek! :)<br>";
+//     echo "Treść komunikatu: ". $e->getMessage() . "<br>";
+//     echo "Treść nazwa pliku: ". $e->getFile() . "<br>";
+//     echo "Treść numer linii: ". $e->getLine() . "<br>";
+//     echo "Treść kod błędu: ". $e->getCode() . "<br>";  
+// }
+
+// niepoprawnie!
+try{
+    throw new OutOfRangeException();
+    }
+    catch(Exception $e){
+    echo "Został wykonany blok dla wyjątku Exception.";
+    }
+    catch(OutOfRangeException $e){
+    echo "Został wykonany blok dla wyjątku OutOfRangeException.";
+    }
+
+
+
+// poprawnie!
+try{
+    throw new OutOfRangeException();
+    }
+    catch(OutOfRangeException $e){
+        echo "Został wykonany blok dla wyjątku OutOfRangeException.";
+        }
+    catch(Exception $e){
+    echo "Został wykonany blok dla wyjątku Exception.";
+    }
+    
+
 
 
 ?>
